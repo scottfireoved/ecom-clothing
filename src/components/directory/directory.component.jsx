9e13797dfa6685b5c363f0af324faa1f192eeece
scottfireoved <scottfireoved@gmail.com>
2019-10-16 23:25:37 -0700

    updated home page menu items.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,35 +9,40 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: 'performance',
+          imageUrl: 'http://www.armoredworks.com/metalcloak/images/2018-Website/Category-Pages/JK/Bumpers/JK-Bumpers.png',
           id: 1,
+          action: 'shop now',
           linkUrl: 'shop/hats'
         },
         {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          title: 'accessories',
+          imageUrl: 'https://cdn3.volusion.com/sxegw.zwlry/v/vspfiles/photos/40333-2.jpg',
           id: 2,
+          action: 'shop now',
           linkUrl: 'shop/jackets'
         },
         {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          title: 'tires',
+          imageUrl: 'https://www.cjponyparts.com/pub/media/images/resource-center/articles/body/wrangler-fuel-off-road-wheels.jpg',
           id: 3,
+          action: 'shop now',
           linkUrl: 'shop/sneakers'
         },
         {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          title: 'new releases',
+          imageUrl: 'https://www.quadratec.com/sites/default/files/styles/product_zoomed/public/product_images/bestop_rhino_rack_for_sunrider_with_hardtop_for_jk_4dr.jpg',
           size: 'large',
           id: 4,
+          action: 'shop now',
           linkUrl: 'shop/womens'
         },
         {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          title: 'forums',
+          imageUrl: 'https://bloximages.newyork1.vip.townnews.com/tylerpaper.com/content/tncms/assets/v3/editorial/4/a7/4a790cf8-ea77-11e7-a198-4b6072ecc983/5a42aaf1e1de7.image.jpg?resize=1200%2C800',
           size: 'large',
           id: 5,
+          action: 'join now',
           linkUrl: 'shop/mens'
         }
       ]
@@ -48,8 +53,8 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ title, imageUrl, id, size, action }) => (
+            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} action={action} />
           ))
         }
       </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, key, size}) => (
+const MenuItem = ({title, imageUrl, key, size, action}) => (
     <div className={`${size} menu-item`}>
         <div className="background-image"
         style={{
             backgroundImage: `url(${imageUrl})`}}/>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
+            <span className="subtitle">{action.toUpperCase()}</span>
         </div>
     </div>
 );
