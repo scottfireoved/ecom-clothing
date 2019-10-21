1e230f09ba058811e7e4480cb119b57fc9efc6bf
scottfireoved <scottfireoved@gmail.com>
2019-10-21 07:24:18 -0700

    Preview shop page progress.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
-const PerformancePage = () => (
-  <div>
-    <h1>Performance Page</h1>
-  </div>
-)
+import ShopPage from './pages/shoppage/shoppage.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/performance" component={PerformancePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
